b289b4b2709171f7265a006c67b269a233fc5a4d
Tzu-Mao Li <tzumao@mit.edu>
2021-12-29 20:52:45 -0800

    Really fix vector max this time...

--- src/vector.h
+++ src/vector.h
@@ -266,8 +266,8 @@ inline T max(const TVector3<T> &v) {
 }
 
 template <typename T>
-inline Vector3<T> max(const TVector3<T> &v0, const TVector3<T> &v1) {
-    return Vector3{max(v0.x, v1.x), max(v0.y, v1.y), max(v0.z, v1.z)};
+inline TVector3<T> max(const TVector3<T> &v0, const TVector3<T> &v1) {
+    return TVector3<T>{max(v0.x, v1.x), max(v0.y, v1.y), max(v0.z, v1.z)};
 }
 
 template <typename T>
