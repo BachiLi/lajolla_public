0cd3b2a19411038b76615b4b611e0c6a13c3d16d
Tzu-Mao Li <tzumao@mit.edu>
2021-12-31 18:02:59 -0800

    Add frame test. Enable tests in CMake

---
+++ src/tests/frame.cpp
@@ -0,0 +1,16 @@
+#include "../frame.h"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    Frame f(normalize(Vector3{0.3, 0.4, 0.5}));
+    Vector3 v = Vector3{-1, -2, -3};
+    Vector3 local_v = to_local(f, v);
+    Vector3 world_v = to_world(f, local_v);
+    if (distance(v, world_v) > Real(1e-3)) {
+        printf("FAIL\n");
+        return 1;
+    }
+
+    printf("SUCCESS\n");
+    return 0;
+}
