f5be64d9b9990de926c67341055fefa186044399
Tzu-Mao Li <tzumao@mit.edu>
2024-01-08 09:40:46 -0800

    Fix FindEmbree CMake file

--- cmake/FindEmbree.cmake
+++ cmake/FindEmbree.cmake
@@ -1,20 +1,20 @@
-find_path(EMBREE_INCLUDE_PATH embree3/rtcore.h
+find_path(EMBREE_INCLUDE_PATH embree4/rtcore.h
   ${CMAKE_SOURCE_DIR}/embree/include
   /usr/include
   /usr/local/include
   /opt/local/include)
 
 if (APPLE)
-find_library(EMBREE_LIBRARY NAMES embree3 PATHS
+find_library(EMBREE_LIBRARY NAMES embree4 PATHS
   ${CMAKE_SOURCE_DIR}/embree/lib-macos
   /usr/lib
   /usr/local/lib
   /opt/local/lib)
 elseif (WIN32)
-find_library(EMBREE_LIBRARY NAMES embree3 PATHS
+find_library(EMBREE_LIBRARY NAMES embree4 PATHS
   ${CMAKE_SOURCE_DIR}/embree/lib-win32)
 else ()
-find_library(EMBREE_LIBRARY NAMES embree3 PATHS
+find_library(EMBREE_LIBRARY NAMES embree4 PATHS
   ${CMAKE_SOURCE_DIR}/embree/lib-linux
   /usr/lib
   /usr/local/lib
